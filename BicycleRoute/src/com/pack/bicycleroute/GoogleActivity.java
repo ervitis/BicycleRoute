4c062b595b7708cc66494882914b943ecb3dba7b
v <vitomarti@gmail.com>
2013-05-28 00:47:09 +0200

    Added new activity classes for google maps and history Edited manifest file Edited strings xml file Added library for plot

---
+++ BicycleRoute/src/com/pack/bicycleroute/GoogleActivity.java
@@ -0,0 +1,14 @@
+package com.pack.bicycleroute;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class GoogleActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_google);
+	}
+
+}
--- BicycleRoute/src/com/pack/bicycleroute/MainActivity.java
+++ BicycleRoute/src/com/pack/bicycleroute/MainActivity.java
@@ -1,22 +1,28 @@
 package com.pack.bicycleroute;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	private static final int REQUEST_MAP = 1;
+	private static final int REQUEST_HISTORY = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	
+	public void onEmpezar(View view){
+		Intent i = new Intent(this, GoogleActivity.class);
+		startActivityForResult(i, REQUEST_MAP);
+	}
+	
+	public void onHistorial(View view){
+		Intent i = new Intent(this, Historial.class);
+		startActivityForResult(i, REQUEST_HISTORY);
 	}
 
 }
